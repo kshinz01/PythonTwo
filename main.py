bf890810620b3dcb1d176fcdd7e10b473bcf647a
KenS <kshinz01@outlook.com>
2021-06-03 06:33:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,56 @@
+def main():
+    pass
 
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def quiz1():
+    # Create a variable called student, with a dictionary.
+    # The dictionary must contain three keys: 'name', 'school', and 'grades'.
+    # The values for each must be 'Jose', 'Computing', and a tuple with the values 66, 77, and 88.
+    student = {'name': 'Jose',
+               'school': 'Computing',
+               'grades': (66, 77, 88,),
+               }
+
+    # Assume the argument, data, is a dictionary.
+    # Modify the grades variable so it accesses the 'grades' key of the data dictionary.
+    def average_grade(data):
+        grades = data['grades']
+        return sum(grades) / len(grades)
+
+    # Implement the function below
+    # Given a list of students (a list of dictionaries), calculate the average grade received on an exam, for the entire class
+    # You must add all the grades of all the students together
+    # You must also count how many grades there are in total in the entire list
+    def average_grade_all_students(student_list):
+        total = 0
+        count = 0
+        for student in student_list:
+            total += sum(student['grades'])
+            count += len(student['grades'])
+
+        return total / count
+
+
+def dictionary_mapping():
+    users = [
+        (0, "Bob", "password"),
+        (1, "Ken", "password"),
+        (2, "Kelly", "password"),
+        (3, "Tester", "password"),
+    ]
+
+    username_mapping = {user[1]: user for user in users}
+
+    username_input = input("Please enter username: ")
+    password_input = input("Please enter password: ")
+
+    _, username, password = username_mapping[username_input]
+
+    if password_input == password:
+        print("OK!")
+    else:
+        print("Wrong name or password.")
 
 
 if __name__ == '__main__':
-    print_hi('Hello World!')
-
+    main()
